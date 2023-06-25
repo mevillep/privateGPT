61f2d05c8dc413fd6e6dcc04f21aa3a0a721250d
Meville <mevillepereira10@gmail.com>
2023-06-25 13:19:39 -0500

    log

--- privateGPT.py
+++ privateGPT.py
@@ -11,15 +11,16 @@ import argparse
 import time
 
 load_dotenv()
-
+# Load the environment variables
 embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
 persist_directory = os.environ.get('PERSIST_DIRECTORY')
-
 model_type = os.environ.get('MODEL_TYPE')
 model_path = os.environ.get('MODEL_PATH')
 model_n_ctx = os.environ.get('MODEL_N_CTX')
 model_n_batch = int(os.environ.get('MODEL_N_BATCH', 8))
 target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS', 4))
+
+# Main function
 
 
 def main():
@@ -70,6 +71,8 @@ def main():
         for document in docs:
             print("\n> " + document.metadata["source"] + ":")
             print(document.page_content)
+
+# Parse the command line arguments
 
 
 def parse_arguments():
